40a695776189030291a33425ee4a9d87f024849b
volirra <127318805+volirra@users.noreply.github.com>
2023-09-28 19:56:46 +0300

    commit

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,3 +1,29 @@
+const BASE_URL = 'https://api.thecatapi.com/v1/';
+const API_KEY =
+  'live_yBI02igIcefpcD2AUW0cwA8QfQTdJY7SV7SyHlO3KuUhh4lZJ1xAzcW2VeCRUnde';
+
+export function fetchBreeds() {
+  return fetch('${BASE_URL}/breeds').then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+    return resp.json();
+  });
+}
+
+export function fetchBreedsById(id) {
+  return fetch(
+    '${BASE_URL}/images/search?breed_ids=${id}api_key=${API_KEY}'
+  ).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+    return resp.json();
+  });
+}
+
+
+
 import axios from 'axios';
 const API_KEY =
   'live_yBI02igIcefpcD2AUW0cwA8QfQTdJY7SV7SyHlO3KuUhh4lZJ1xAzcW2VeCRUnde';
--- src/index.js
+++ src/index.js
@@ -1,58 +1,11 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
 const breedSelect = document.querySelector('.breed-select');
-const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
-const catImage = document.querySelector('.cat-image');
-const catName = document.querySelector('.cat-name');
-const catDescription = document.querySelector('.cat-description');
-const catTemperament = document.querySelector('.cat-temperament');
 
-async function populateBreeds() {
-  try {
-    const breeds = await fetchBreeds();
-    breeds.forEach(breed => {
-      const option = document.createElement('option');
-      option.value = breed.id;
-      option.textContent = breed.name;
-      breedSelect.appendChild(option);
-    });
-  } catch (err) {
-    console.error('Error fetching breeds:', err);
-    showError();
-  }
+breedSelect.addEventListener('change, changeBreed')
+
+function changeBreed(e) {
+  const id = e.currentTarget.value;
+  
 }
-
-async function displayCatInfo(breedId) {
-  try {
-    const catData = await fetchCatByBreed(breedId);
-    const cat = catData[0];
-    catImage.src = cat.url;
-    catName.textContent = cat.breeds[0].name;
-    catDescription.textContent = cat.breeds[0].description;
-    catTemperament.textContent = `Temperament: ${cat.breeds[0].temperament}`;
-    catInfo.style.display = 'block';
-  } catch (err) {
-    console.error('Error fetching cat info:', err);
-    showError();
-  }
-}
-
-function hideLoader() {
-  loader.style.display = 'none';
-}
-
-function showError() {
-  error.style.display = 'block';
-}
-
-breedSelect.addEventListener('change', event => {
-  const breedId = event.target.value;
-  catInfo.style.display = 'none';
-  loader.style.display = 'block';
-  error.style.display = 'none';
-  displayCatInfo(breedId).then(hideLoader);
-});
-
-populateBreeds().then(hideLoader);
