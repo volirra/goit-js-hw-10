af3e1a2b972206d7d4faf089ba6d501d6e70d360
volirra <127318805+volirra@users.noreply.github.com>
2023-09-28 18:59:39 +0300

    commit

---
+++ src/cat-api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+const API_KEY =
+  'live_yBI02igIcefpcD2AUW0cwA8QfQTdJY7SV7SyHlO3KuUhh4lZJ1xAzcW2VeCRUnde';
+axios.defaults.headers.common['x-api-key'] = API_KEY;
+
+export async function fetchBreeds() {
+  try {
+    const response = await axios.get('https://api.thecatapi.com/v1/breeds');
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function fetchCatByBreed(breedId) {
+  try {
+    const response = await axios.get(
+      `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
