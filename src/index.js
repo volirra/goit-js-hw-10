d48874a8ece8b5e9f1dff8ed4e3500bc3e38094a
volirra <127318805+volirra@users.noreply.github.com>
2023-09-29 20:48:19 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,66 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import axios from "axios";
+axios.defaults.headers.common['x-api-key'] =
+  'live_yBI02igIcefpcD2AUW0cwA8QfQTdJY7SV7SyHlO3KuUhh4lZJ1xAzcW2VeCRUnde';
+import {fetchBreeds, fetchCatByBreed} from './cat-api'
 
 const breedSelect = document.querySelector('.breed-select');
+
+const infoLoader = document.querySelector('.loader');
+const selectError = document.querySelector('.error');
+const catInfo = document.querySelector('.cat-info');
+
+window.addEventListener('load', init);
+
+function init(){
+    let breedsData;
+    fetchBreeds()
+    .then(data => {
+        breedsData = data;
+        data.forEach(breed => {
+            const option = document.createElement('option');
+            option.value = breed.id;
+            option.textContent = breed.name;
+            breedSelect.appendChild(option);
+
+            breedSelect.classList.remove('is-hidden');
+            infoLoader.classList.add('is-hidden');
+          })
+    })
+    .catch(error => {
+        console.log(error);
+        selectError.classList.remove('is-hidden');
+        breedSelect.classList.add('is-hidden');
+        infoLoader.classList.add('is-hidden');
+    })
+
+    breedSelect.addEventListener('change', () => {
+        const selectBreedId = breedSelect.value;
+        infoLoader.classList.remove('is-hidden');
+        catInfo.classList.add('is-hidden');
+        selectError.classList.add('is-hidden');
+
+        fetchCatByBreed(selectBreedId)
+        .then(result => {
+            const catData = result[0];
+            const breedData = breedsData.find(breed => breed.id === catData.breeds[0].id);
+
+            const markup = createMarkup(catData, breedData);
+            catInfo.innerHTML = markup;
+
+            infoLoader.classList.add('is-hidden');
+            catInfo.classList.remove('is-hidden');
+        })
+        .catch(error => {
+            console.log(error);
+            selectError.classList.remove('is-hidden');
+            infoLoader.classList.add('is-hidden');
+        })
+    });
+}
+
+        function createMarkup(catData, breedData){
+        return `<img src='${catData.url}' width='300' alt='${breedData.name}'/>
+        <div class='textInfo'><h1>${breedData.name}</h1>
+        <p>${breedData.description}</p>
+        <p><b>Temperament:</b> ${breedData.temperament}</p></div>`
+    }
