ccb48b7c731128dcb5ec14e74eeb8bdfd5191412
volirra <127318805+volirra@users.noreply.github.com>
2023-09-30 17:25:06 +0300

    commit

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,7 +3,7 @@ const API_KEY =
 const BASE_URL = 'https://api.thecatapi.com/v1/';
 
 export function fetchBreeds() {
-  return fetch(`${BASE_URL}/breeds`)
+  return fetch(`${BASE_URL}breeds`)
     .then(response => {
     if (!response.ok) {
       throw new Error(response.status);
